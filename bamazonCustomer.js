dbc532ae30aa62dc7a647edde7d8fb9f98b3bc2b
reb2039607 <reb.aguayo@gmail.com>
2019-08-12 23:02:34 -0700

    prompts added, need to link response to table to select item/check quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,6 +15,34 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  // console.log("connected as id " + connection.threadId);
 });
 
+inquirer
+  .prompt([
+    
+    {
+      type: "input",
+      message: "Please enter the ID of the product you would like to purchase:",
+      name: "product"
+    },
+        
+    {
+      type: "input", 
+      message: "How many would you like?",
+      name: "quantity"
+    }
+  ])
+  
+  .then(function(inquirerResponse) {
+    
+    if (inquirerResponse.product) {
+      
+
+      console.log("\nYou have selected " + inquirerResponse.quantity + inquirerResponse.product);
+     
+    }
+    else {
+      console.log("\nSorry you didn't find what you were looking for.\n");
+    }
+  });
